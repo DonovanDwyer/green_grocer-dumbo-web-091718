c2ca32523311eb18526b113acdf2ad8cb71edd16
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-13 23:07:10 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = Hash.new(0)
+  array = []
+  cart.each {|h| new_hash[h] += 1}
+  new_hash.each do |key, value|
+    key.each do |x,y|
+      y[:count] = new_hash[key]
+    end
+    array << key
+  end
+  array = array.inject(:merge!)
+  #binding.pry
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.map do |couphash|
+    if cart.has_key? couphash[:item]
+      cart[couphash[:item]][:count] -= couphash[:num]
+      cart["#{couphash[:item]} W/COUPON"] = {
+        :price => couphash[:cost],
+        :clearance => cart[couphash[:item]][:clearance],
+        :count => couphash.count
+      }
+      binding.pry
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)

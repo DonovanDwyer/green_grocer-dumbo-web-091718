9a160f6154db5ccb3ab51d0111bccb4818a115db
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-13 23:59:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.map do |couphash|
     if cart.has_key?("#{couphash[:item]} W/COUPON")
-      cart[couphash[:item]][:count] += 1
+      cart["#{couphash[:item]} W/COUPON"][:count] += 1
     elsif cart.has_key? couphash[:item]
       cart[couphash[:item]][:count] -= couphash[:num]
       cart["#{couphash[:item]} W/COUPON"] = {

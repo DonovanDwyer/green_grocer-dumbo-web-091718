aeff5e12280c7e3a091f3e19cb1c0fdc1391328a
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 00:59:32 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -26,7 +26,7 @@ def apply_coupons(cart, coupons)
         :clearance => cart[couphash[:item]][:clearance],
         :count => 1
       }
-    if cart[couphash[:item]][:count] <= 0 then 
+    if cart[couphash[:item]][:count] <= 0 then cart
       binding.pry
     end
   end
